98bed04553c6b3b7c6b270a29001ee294f3e89cb
JohKing <huynhjohn2021@gmail.com>
2020-11-03 21:17:58 +0000

    johns commit

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,6 @@
 int main() {
   std::cout << "Jonathan was here" << std::endl;
   std::cout << "hi john was here" << std::endl;
-  std::cout << "not nice johns branch" << std::endl;
+  std::cout << "whatever not nice johns branch" << std::endl;
     return 0;
 }
